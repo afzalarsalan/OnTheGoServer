0d54a2d5c5ea295dea934ef68ff5fb4ae6a5f01c
Arsalan Afzal <afzal.arsalan@gmail.com>
2016-10-29 23:02:11 -0500

    Let's try eliminating bad data

--- app.js
+++ app.js
@@ -60,7 +60,9 @@ function callGoogleAPI(imageData) {
       console.log('textAnnotation Result', textAnnotation.split('\n'));
       textAnnotation.split('\n').forEach(function (val) {
         imdb.getReq({ name: val, year: 2016 }, function (err, data) {
+          if(typeof data === imdb.Movie) {
           console.log(data);
+          }
         });
       });
     }
