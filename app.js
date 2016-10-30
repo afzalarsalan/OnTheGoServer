59c9a98c3ce1cfac8ed8ad4dd2a5f4f3fa24b964
Arsalan Afzal <afzal.arsalan@gmail.com>
2016-10-29 22:30:52 -0500

    Forget promises

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var vision = require('node-cloud-vision-api');
-vision.init({auth: 'AIzaSyCbDDuE_7XnbPTfJtMhgWWETzQTcnpKRlY'});
+vision.init({ auth: 'AIzaSyCbDDuE_7XnbPTfJtMhgWWETzQTcnpKRlY' });
 
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 var fs = require("fs"),
-    http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    imdb = require("imdb-api");
+  http = require("http"),
+  url = require("url"),
+  path = require("path"),
+  imdb = require("imdb-api");
 
 var visual_recognition = new VisualRecognitionV3({
   api_key: '513bbcd9cbce91d28ba03fd2492cde32f3b66f9a'/*'37745a59c469e745974eac39b50a52752a803887'*/,
@@ -14,26 +14,26 @@ var visual_recognition = new VisualRecognitionV3({
 });
 
 http.createServer(function (request, response) {
-   if (request.method == 'POST' && request.url.indexOf('/index') > -1) {
-      var body = '';
-      request.on('data', function (data) {
-          body += data;
-          if (body.length > 1e6)
-              request.connection.destroy();
-      });
-      request.on('end', function () {
-          var postTemp = JSON.parse(body);
-          //console.log('post.imageData:',postTemp.imageData);
-          callGoogleAPI(postTemp.imageData);
-          response.writeHead(200, {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "X-Requested-With"});
-          response.write('Hello');
-          response.end();
-      });
+  if (request.method == 'POST' && request.url.indexOf('/index') > -1) {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+      if (body.length > 1e6)
+        request.connection.destroy();
+    });
+    request.on('end', function () {
+      var postTemp = JSON.parse(body);
+      //console.log('post.imageData:',postTemp.imageData);
+      callGoogleAPI(postTemp.imageData);
+      response.writeHead(200, { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "X-Requested-With" });
+      response.write('Hello');
+      response.end();
+    });
   }
-}).listen(process.env.PORT||8888);
+}).listen(process.env.PORT || 8888);
 
 
-function callGoogleAPI(imageData){
+function callGoogleAPI(imageData) {
   console.log('Inside callGoogleAPI');
   var req = new vision.Request({
     image: new vision.Image({
@@ -51,20 +51,20 @@ function callGoogleAPI(imageData){
     // handling response
     console.log(JSON.stringify(res.responses))
     var resultObject = res.responses[0];
-    if(resultObject.logoAnnotations){
+    if (resultObject.logoAnnotations) {
       var logoAnnotation = resultObject.logoAnnotations;
-      console.log('logoAnnotation',logoAnnotation);
+      console.log('logoAnnotation', logoAnnotation);
     }
-    if(resultObject.textAnnotations){
+    if (resultObject.textAnnotations) {
       var textAnnotation = resultObject.textAnnotations[0].description;
-      console.log('textAnnotation Result',textAnnotation.split('\n'));
+      console.log('textAnnotation Result', textAnnotation.split('\n'));
       textAnnotation.split('\n').forEach(function (val) {
-        imdb.getReq({name: val, year: 2016}).then(function (data) {
-          if(typeof data === imdb.Movie) {
+        imdb.getReq({ name: val, year: 2016 }, function (data) {
+          if (typeof data === imdb.Movie) {
             console.log(data);
           }
-        })
-      })
+        });
+      });
     }
   }, (e) => {
     console.log('Error: ', e)
