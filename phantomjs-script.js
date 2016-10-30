98e00d9c6f2a3eb6f7acec5f3f9dcc2f4986d557
Karunanidhi Devanidhi <karunchillz@Karunanidhis-MacBook-Air.local>
2016-10-30 09:27:02 -0500

    changes

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -20,7 +20,7 @@ console.log(status);
 page.evaluate(function(){
 //window.onload = init();
 console.log("Program entered");
-document.getElementById('mce-EMAIL').value="karun.chillz@gmail.com";
+document.getElementById('mce-EMAIL').value="karun.devanidhi@tamu.edu";
 //$("#mce-EMAIL").val="karun.devanidhi@tamu.edu";
 //console.log($("mce-EMAIL").val);
 //document.getElementById("mce-FNAME").value="karun.devanidhi@tamu.edu";
